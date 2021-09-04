138f7b53524ea6663fb5dbfba975653c71f63214
huhao <18255606178@163.com>
2021-09-04 17:28:24 +0800

    更新

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ function gitCommit(time) {
 		gitEntity
 		.add('./*')
 		.commit('更新图片' + time)
-		.pull('origin', 'branch')
 		.push(['-u', 'origin', 'master'], (e) => {
 			console.log('commit 成功，时间：' + time, e)
 		})
