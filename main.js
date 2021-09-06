7a956e5a418f51f1e36b0cc80954dd13b7f9012f
huhao <18255606178@163.com>
2021-09-06 08:41:26 +0800

    更新图片Mon Sep 06 2021 08:41:26 GMT+0800 (GMT+08:00)

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ setInterval(function() {
 	upDataFile()
 }, 30000) // 30s检查一次，本地是否有修改
 
-// 修改 README 文件
+// 提交
 upDataFile()
 
 function upDataFile() {
@@ -24,18 +24,18 @@ function upDataFile() {
 // commit 提交
 function gitCommit(time) {
 	gitEntity.status().then(res=>{
-		
 		const not_added = res.not_added.length === 0;
 		const created = res.created.length === 0;
 		const deleted = res.deleted.length === 0;
 		const modified = res.modified.length === 0;
 		const renamed = res.renamed.length === 0;
     if(not_added && created && deleted && modified && renamed)return false;
+		
 		gitEntity
 		.add('./*')
 		.commit('更新' + time)
 		.push(['-u', 'origin', 'master'], (e) => {
-			console.log('commit 成功，时间：' + time, e)
+			console.log('commit 成功，时间：' + time)
 		})
 	})
 }
