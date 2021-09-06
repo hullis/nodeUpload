ec160cc35bbcada54f396c50e2e6e9385c20cba2
huhao <18255606178@163.com>
2021-09-06 08:39:26 +0800

    更新图片Mon Sep 06 2021 08:39:25 GMT+0800 (GMT+08:00)

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const git = require('simple-git')
 /**
  * 初始化git
  */
-let gitEntity = git("F:/www/my/tuchuang/vuepress")
+let gitEntity = git("F:/www/my/tuchuang")
 
 // 定时器
 setInterval(function() {
 	upDataFile()
-}, 60000) // 一分钟检查一次，本地是否有修改
+}, 30000) // 30s检查一次，本地是否有修改
 
 // 修改 README 文件
 upDataFile()
@@ -24,7 +24,13 @@ function upDataFile() {
 // commit 提交
 function gitCommit(time) {
 	gitEntity.status().then(res=>{
-    if(res.not_added.length === 0)return false;
+		
+		const not_added = res.not_added.length === 0;
+		const created = res.created.length === 0;
+		const deleted = res.deleted.length === 0;
+		const modified = res.modified.length === 0;
+		const renamed = res.renamed.length === 0;
+    if(not_added && created && deleted && modified && renamed)return false;
 		gitEntity
 		.add('./*')
 		.commit('更新图片' + time)
