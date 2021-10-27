987b2658c95ffbbf0d7a1f2137915a643e4e6f5b
huhao <18255606178@163.com>
2021-10-27 13:46:03 +0800

    更新Wed Oct 27 2021 13:46:02 GMT+0800 (中国标准时间)

--- http.js
+++ http.js
@@ -1,4 +1,7 @@
 'use strict';
+const fs = require('fs')
+const git = require('simple-git')
+
 
 var express = require('express'),
     path = require('path'),
@@ -39,6 +42,35 @@ var upload = multer({
     storage: storage
 });
 
+
+/**
+ * 初始化git
+ */
+let gitEntity = git("D:/www/nodeUpload")
+
+function upDataFile() {
+    const time = Date()
+    gitCommit(time)
+}
+// commit 提交
+function gitCommit(time) {
+    gitEntity.status().then(res => {
+        const not_added = res.not_added.length === 0;
+        const created = res.created.length === 0;
+        const deleted = res.deleted.length === 0;
+        const modified = res.modified.length === 0;
+        const renamed = res.renamed.length === 0;
+        if (not_added && created && deleted && modified && renamed) return false;
+
+        gitEntity
+            .add('./*')
+            .commit('更新' + time)
+            .push(['-u', 'origin', 'master'], (e) => {
+                console.log('commit 成功，时间：' + time)
+            })
+    })
+}
+
 //单文件上传，多文件上传参考https://github.com/expressjs/multer
 app.post('/upload', upload.single('file'), function (req, res) {
 
@@ -63,4 +95,12 @@ app.post('/upload', upload.single('file'), function (req, res) {
 
 app.listen(config.port, config.host, function () {
     console.log('Server start on ' + config.host + ':' + config.port);
+
+    // 定时器
+    setInterval(function () {
+        upDataFile()
+    }, 3000000) // 检查一次，本地是否有修改，避免过多commit信息
+
+    // 提交
+    upDataFile()
 });
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const git = require('simple-git')
 let gitEntity = git("D:/www/nodeUpload")
 
 // 定时器
-setInterval(function() {
+setInterval(function () {
 	upDataFile()
 }, 3000000) // 检查一次，本地是否有修改，避免过多commit信息
 
@@ -20,22 +20,21 @@ function upDataFile() {
 	const time = Date()
 	gitCommit(time)
 }
-
 // commit 提交
 function gitCommit(time) {
-	gitEntity.status().then(res=>{
+	gitEntity.status().then(res => {
 		const not_added = res.not_added.length === 0;
 		const created = res.created.length === 0;
 		const deleted = res.deleted.length === 0;
 		const modified = res.modified.length === 0;
 		const renamed = res.renamed.length === 0;
-    if(not_added && created && deleted && modified && renamed)return false;
-		
+		if (not_added && created && deleted && modified && renamed) return false;
+
 		gitEntity
-		.add('./*')
-		.commit('更新' + time)
-		.push(['-u', 'origin', 'master'], (e) => {
-			console.log('commit 成功，时间：' + time)
-		})
+			.add('./*')
+			.commit('更新' + time)
+			.push(['-u', 'origin', 'master'], (e) => {
+				console.log('commit 成功，时间：' + time)
+			})
 	})
 }
